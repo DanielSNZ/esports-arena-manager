643b6920cc97f1180e1f597c0084f1a3c4dfa6fa
Daniel Saa <danielsaa22@gmail.com>
2026-06-19 19:37:47 -0400

    Agregar Swagger en microservicios

---
+++ game-service/src/main/java/com/esports/game_service/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.esports.game_service.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Juegos")
+                        .version("1.0")
+                        .description("Gestión de juegos del sistema Esports Arena Manager"))
+                .components(new Components().addSecuritySchemes(
+                        "bearer-jwt",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
